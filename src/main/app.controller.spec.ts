549db50d1be902ba3279da20940cbfe490de63ab
Luis Sanchez <luis.sanchez.dev@hotmail.com>
2022-09-04 00:42:49 -0500

    Fix unit tests

--- src/main/app.controller.spec.ts
+++ src/main/app.controller.spec.ts
@@ -18,9 +18,5 @@ describe('AppController', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toMatch(/Hello World! \d{13}/);
     });
-
-    it('asd', () => {
-      expect(true).toBeFalsy();
-    });
   });
 });
